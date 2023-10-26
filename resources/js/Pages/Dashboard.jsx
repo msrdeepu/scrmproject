baa3574be7e677bace8b6fa8961b23afc7f49238
msrdeepu <mspreddy2021@gmail.com>
2023-10-26 18:17:27 +0530

    changes

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -3,20 +3,11 @@ import { Head } from "@inertiajs/react";
 
 export default function Dashboard({ auth }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={
-                <div className="flex">
-                    <h2 className="font-semibold text-xl text-gray-800 leading-tight m-3">
-                        Dashboard
-                    </h2>
-                </div>
-            }
-        >
+        <AuthenticatedLayout user={auth.user}>
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-8xl mx-auto sm:px-6 lg:px-8">
+            <div className="py-2">
+                <div className="w-[100%] mx-auto p-2">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             You're logged in!
